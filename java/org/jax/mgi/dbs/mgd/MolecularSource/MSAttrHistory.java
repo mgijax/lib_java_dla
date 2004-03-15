4ae4f8ca3aeb91b10f9cfbce7ef44ae3e0753f7d
mbw <>
2004-03-15 21:21:40 +0000

    now extends AttrHistory

--- java/org/jax/mgi/dbs/mgd/MolecularSource/MSAttrHistory.java
+++ java/org/jax/mgi/dbs/mgd/MolecularSource/MSAttrHistory.java
@@ -9,6 +9,7 @@ import org.jax.mgi.shr.config.ConfigException;
 import org.jax.mgi.dbs.mgd.MGD;
 import org.jax.mgi.dbs.SchemaConstants;
 import org.jax.mgi.dbs.mgd.MGITypeConstants;
+import org.jax.mgi.shr.dla.seqloader.AttrHistory;
 
 /**
  * @is an object for looking up data in the mgi_attributeHistory table for
@@ -21,46 +22,8 @@ import org.jax.mgi.dbs.mgd.MGITypeConstants;
  * @version 1.0
  */
 
-public class MSAttrHistory
+public class MSAttrHistory extends AttrHistory
 {
-    /**
-     * object for getting access to the database
-     */
-    protected SQLDataManager sqlMgr = null;
-
-    /**
-     * the query statement which allows binding of a source key and column
-     * name to a query to the mgi_attributeHistory table at runtime
-     */
-    protected BindableStatement query = null;
-
-    /**
-     * the query string for the mgi_attributeHistory table which returns a
-     * history record if it was modified by any user other than a user
-     * of type 'Data Loads'
-     */
-    protected String sql =
-        "SELECT 1 " +
-        "FROM " +
-            MGD.mgi_attributehistory._name + " his, " +
-            MGD.mgi_user._name + " usr, " +
-            MGD.voc_term._name + " trm " +
-        "WHERE his." + MGD.mgi_attributehistory._mgitype_key + " = " +
-            MGITypeConstants.SOURCE + " " +
-        "AND his." + MGD.mgi_attributehistory.columnname + " = ? " +
-        "AND his." + MGD.mgi_attributehistory._object_key + " = ? " +
-        "AND his." + MGD.mgi_attributehistory._modifiedby_key + " = " +
-            "usr." + MGD.mgi_user._user_key + " " +
-        "AND usr." + MGD.mgi_user._usertype_key + " = " +
-            "trm." + MGD.voc_term._term_key + " " +
-        "AND trm." + MGD.voc_term.term + " != '" + DATALOAD_USER + "'";
-
-    /**
-     * constant definition for the voc_term value which
-     * is used to join voc_term to mgi_user to identify a dataloads user
-     */
-    protected static final String DATALOAD_USER = "Data Load";
-
     /**
      * contants for prb_source column names
      */
@@ -71,8 +34,6 @@ public class MSAttrHistory
     protected static final String GENDER_COL = MGD.prb_source._gender_key;
     protected static final String AGE_COL = MGD.prb_source.age;
 
-
-
     /**
      * constructor
      * @throws ConfigException thrown if there is an error with the database
@@ -81,8 +42,7 @@ public class MSAttrHistory
     public MSAttrHistory()
     throws ConfigException, DBException
     {
-        sqlMgr = SQLDataManagerFactory.getShared(SchemaConstants.MGD);
-        query = sqlMgr.getBindableStatement(sql);
+        super(MGITypeConstants.SOURCE);
     }
 
     /**
@@ -167,34 +127,6 @@ public class MSAttrHistory
     throws DBException
     {
       return isCurated(ORGANISM_COL, sourceKey);
-    }
-
-    /**
-     * performs the database query for a given column name and source key
-     * to see if a column has been curator edited
-     * @param columnName the name of the column from the prb_source table
-     * @param sourceKey the source key for the MolecularSource object
-     * @return true if the attribute has been curator edited; false otherwise
-     * @throws DBException thrown if there is an error with the database
-     */
-    protected boolean isCurated(String columnName, Integer sourceKey)
-    throws DBException
-    {
-        /**
-         * bind parameters to the query
-         */
-        query.setString(1, columnName);
-        query.setInt(2, sourceKey.intValue());
-
-        /**
-         * execute the query
-         */
-        ResultsNavigator nav = query.executeQuery();
-        boolean found = false;
-        if (nav.next())  // indicates records were found in history
-            found = true;
-        nav.close();
-        return found;
     }
 
 
