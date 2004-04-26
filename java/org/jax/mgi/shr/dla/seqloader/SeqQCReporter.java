ab106fff0e4f470f0c04d35432185fd4a3c602ab
Sharon Giannatto <Sharon.Giannatto@jax.org>
2004-04-26 12:24:27 +0000

    changed QC_SEQ_RawSourceConflict column names

--- java/org/jax/mgi/shr/dla/seqloader/SeqQCReporter.java
+++ java/org/jax/mgi/shr/dla/seqloader/SeqQCReporter.java
@@ -7,6 +7,8 @@ import org.jax.mgi.dbs.rdr.dao.QC_SEQ_OldRefDAO;
 import org.jax.mgi.dbs.rdr.dao.QC_SEQ_OldRefState;
 import org.jax.mgi.dbs.rdr.dao.QC_SEQ_RawSourceConflictState;
 import org.jax.mgi.dbs.rdr.dao.QC_SEQ_RawSourceConflictDAO;
+import org.jax.mgi.dbs.rdr.dao.QC_SEQ_MergedDAO;
+import org.jax.mgi.dbs.rdr.dao.QC_SEQ_MergedState;
 import org.jax.mgi.shr.dbutils.dao.SQLStream;
 import org.jax.mgi.shr.exception.MGIException;
 
@@ -44,15 +46,12 @@ public class SeqQCReporter
      * @throws SeqloaderException
      */
 
-    public SeqQCReporter(SQLStream stream)
-    {
+    public SeqQCReporter(SQLStream stream) {
       this.stream = stream;
     }
 
     public void reportOldReferences(Integer sequenceKey, Integer refsKey)
-        throws SeqloaderException
-
-    {
+        throws SeqloaderException {
         QC_SEQ_OldRefState state = new QC_SEQ_OldRefState();
         state.setSequenceKey(sequenceKey);
         state.setRefsKey(refsKey);
@@ -69,27 +68,40 @@ public class SeqQCReporter
       }
     }
 
-    public void reportRawSourceConflicts(Integer sequenceKey, String rawOrganism, String rawLibrary)
-        throws SeqloaderException
-
-    {
+    public void reportRawSourceConflicts(Integer sequenceKey, String attrName,
+                                         String incomingValue)
+        throws SeqloaderException {
         QC_SEQ_RawSourceConflictState state = new QC_SEQ_RawSourceConflictState();
         state.setSequenceKey(sequenceKey);
+        state.setAttrName(attrName);
+        state.setIncomingValue(incomingValue);
 
-        try
-        {
+        try{
             stream.insert(new QC_SEQ_RawSourceConflictDAO(state));
         }
-        catch (MGIException e)
-        {
+        catch (MGIException e){
             SeqloaderExceptionFactory eFactory = new SeqloaderExceptionFactory();
             SeqloaderException e2 = (SeqloaderException)eFactory.getException(QCErr, e);
-            e2.bind("QC_SEQ_OldRef");
+            e2.bind("QC_SEQ_RawSourceConflict");
             throw e2;
       }
     }
-    public void reportMergedSeqs(String fromSeqid, String toSeqid) {
 
+    public void reportMergedSeqs(String fromSeqid, String toSeqid)
+              throws SeqloaderException {
+        QC_SEQ_MergedState state = new QC_SEQ_MergedState();
+        state.setFromSeqId(fromSeqid);
+        state.setToSeqId(toSeqid);
+
+        try {
+            stream.insert(new QC_SEQ_MergedDAO(state));
+        }
+        catch (MGIException e){
+            SeqloaderExceptionFactory eFactory = new SeqloaderExceptionFactory();
+            SeqloaderException e2 = (SeqloaderException) eFactory.getException(QCErr,
+                e);
+            e2.bind("QC_SEQ_Merged");
+            throw e2;
+        }
     }
-
 }
