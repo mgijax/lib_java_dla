ffa591f2e68825336ba21205d5cf4a7b6e19fac2
mbw <>
2004-08-04 14:41:20 +0000

    initial version

---
+++ java/org/jax/mgi/shr/dla/DLALoaderHelper.java
@@ -0,0 +1,35 @@
+package org.jax.mgi.shr.dla;
+
+import org.jax.mgi.shr.dbutils.DBException;
+import org.jax.mgi.shr.dbutils.DBSchema;
+
+/**
+ * A collection of helper functions for the DLALoader class
+ * @has a collection of loader helper functions
+ * @does nothing
+ * @company The Jackson Laboratory</p>
+ * @author MWalker
+ *
+ */
+
+public class DLALoaderHelper
+{
+
+      /**
+       * truncate the list of tables
+       * @param list the list of tables to truncate
+       * @assumes nothing
+       * @effects the list of tables will be truncated
+       * @throws DBException thrown if there is an error accessing the database
+       */
+      static public void truncateTables(String[] list, DBSchema schema)
+      throws DBException
+      {
+          for (int i = 0; i < list.length; i++)
+          {
+              schema.truncateTable(list[i]);
+          }
+      }
+
+
+}
