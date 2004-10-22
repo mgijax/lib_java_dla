5df09a1bd5317d7b1948d765390e985af2e4f06d
CVS 2 Git Conversion <mgiadmin@jax.org>
2004-10-22 17:14:08 +0000

    This commit was manufactured by cvs2svn to create branch 'assembly'.
    Cherrypick from master 2004-10-22 17:14:07 UTC Sharon Giannatto <Sharon.Giannatto@jax.org> 'renamed from MGSCoordMapFormatInterpreter.java':
        README
        java/org/jax/mgi/shr/dla/DLALoaderHelper.java
        java/org/jax/mgi/shr/dla/DLALoggerFactory.java
        java/org/jax/mgi/shr/dla/coordloader/CoordinateInterpreter.java
        java/org/jax/mgi/shr/dla/coordloader/MGSCoordinateFormatInterpreter.java

---
+++ java/org/jax/mgi/shr/dla/DLALoggerFactory.java
@@ -0,0 +1,39 @@
+package org.jax.mgi.shr.dla;
+
+import org.jax.mgi.shr.log.LoggerFactory;
+import org.jax.mgi.shr.log.Logger;
+
+/**
+ * @is a LoggerFactory for creating a DLALogger
+ * @has nothing
+ * @does creates an instance of a DLALogger
+ * @company The Jackson Laboratory
+ * @author M Walker
+ *
+ */
+
+public class DLALoggerFactory implements LoggerFactory
+{
+
+    /**
+     * get the Logger implementation of the DLALogger
+     * @assumes nothing
+     * @effects nothing
+     * @return this instance as an implementation of Logger
+     */
+    public Logger getLogger()
+    {
+        Logger instance = null;
+        try
+        {
+            instance = (Logger)DLALogger.getInstance();
+        }
+        catch (DLALoggingException e)
+        {
+            // just return null without throwing an exception
+        }
+        return instance;
+    }
+
+
+}
