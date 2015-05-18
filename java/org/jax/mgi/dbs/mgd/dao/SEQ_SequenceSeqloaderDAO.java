52ac7a2d85b95d3eb3fff4afab75366642665842
MGI Admin <mgiadmin@jax.org>
2015-05-18 11:52:39 +0000

    fixed SEQ_deleteDummy call

--- java/org/jax/mgi/dbs/mgd/dao/SEQ_SequenceSeqloaderDAO.java
+++ java/org/jax/mgi/dbs/mgd/dao/SEQ_SequenceSeqloaderDAO.java
@@ -6,6 +6,7 @@ package org.jax.mgi.dbs.mgd.dao;
 import org.jax.mgi.shr.config.ConfigException;
 import org.jax.mgi.shr.dbutils.DBException;
 import org.jax.mgi.shr.types.Converter;
+import org.jax.mgi.shr.dla.loader.seq.SeqloaderConstants;
 
 /**
  * An object that represents a record in the SEQ_Sequence table. Overrides
@@ -61,10 +62,15 @@ public class SEQ_SequenceSeqloaderDAO extends SEQ_SequenceDAO{
        * @throws DBException
        */
        public String getDeleteSQL() {
-            StringBuffer sql = new StringBuffer("SEQ_deleteDummy ");
+            StringBuffer sql = new StringBuffer("select * from SEQ_deleteDummy ");
             // Following for BCP_Inline_Stream
             //StringBuffer sql = new StringBuffer ("call SEQ_DeleteDummy " );
-            sql.append(Converter.toSQLString(this.getKey().getKey()));
+            //sql.append(Converter.toSQLString(this.getKey().getKey()));
+	    sql.append(SeqloaderConstants.OPEN_PAREN);
+	    sql.append(SeqloaderConstants.SGL_QUOTE);
+	    sql.append(this.getKey().getKey());
+	    sql.append(SeqloaderConstants.SGL_QUOTE);
+	    sql.append(SeqloaderConstants.CLOSE_PAREN);
             return new String(sql);
        }
 }
