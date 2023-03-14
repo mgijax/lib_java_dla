d3623445103646f167b13e305b43c492258911ef
Sharon Cousins Giannatto <sharon.giannatto@jax.org>
2023-03-14 09:49:24 -0400

    Added NCBISequenceLookup, if NCBI Gene Model collection use this lookup, otherwise use AccessionLookup. NCBISequenceLookup returns hashmap of sequenceKey and chromosome (from the sequence description). so we can hook up coordinate to the correct sequence

--- java/org/jax/mgi/dbs/mgd/loads/Coord/CoordMapFeatureResolver.java
+++ java/org/jax/mgi/dbs/mgd/loads/Coord/CoordMapFeatureResolver.java
@@ -1,7 +1,11 @@
 package org.jax.mgi.dbs.mgd.loads.Coord;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 import org.jax.mgi.shr.config.CoordLoadCfg;
 import org.jax.mgi.dbs.mgd.dao.MAP_Coord_FeatureState;
+import org.jax.mgi.dbs.mgd.loads.Seq.NCBISequenceLookup;
 import org.jax.mgi.dbs.mgd.lookup.AccessionLookup;
 import org.jax.mgi.dbs.mgd.lookup.LogicalDBLookup;
 import org.jax.mgi.dbs.mgd.lookup.MGITypeLookup;
@@ -10,6 +14,8 @@ import org.jax.mgi.shr.dbutils.DBException;
 import org.jax.mgi.shr.cache.CacheException;
 import org.jax.mgi.shr.cache.KeyNotFoundException;
 import org.jax.mgi.shr.config.ConfigException;
+import org.jax.mgi.shr.dla.log.DLALogger;
+import org.jax.mgi.shr.dla.log.DLALoggingException;
 
 
 /**
@@ -35,9 +41,13 @@ public class CoordMapFeatureResolver {
     // to resolve the object key
     private AccessionLookup accLookup;
 
+    // to resolve the object keys for NCBI Gene Models
+    private NCBISequenceLookup ncbiLookup;
+
     // resolved MGIType key
     private Integer MGITypeKey;
 
+    DLALogger logger;
     /**
      * Constructs a CoordMapFeatureResolver object
      * @throws DBException
@@ -47,13 +57,15 @@ public class CoordMapFeatureResolver {
      */
 
     public CoordMapFeatureResolver() throws DBException,
-            CacheException, KeyNotFoundException, ConfigException{
+            CacheException, KeyNotFoundException, ConfigException,  DLALoggingException{
         coordCfg = new CoordLoadCfg();
         Integer logicalDBKey = new LogicalDBLookup().lookup(
             coordCfg.getLogicalDB());
         MGITypeKey = new MGITypeLookup().lookup(coordCfg.getFeatureMGIType());
         accLookup = new AccessionLookup(logicalDBKey.intValue(),
             MGITypeKey.intValue(), AccessionLib.PREFERRED);
+        ncbiLookup  = new NCBISequenceLookup();
+        logger = DLALogger.getInstance();
     }
 
     /**
@@ -70,16 +82,36 @@ public class CoordMapFeatureResolver {
      */
 
     public MAP_Coord_FeatureState resolve(CoordMapFeatureRawAttributes rawAttr,
-                                          Integer mapKey)
-            throws DBException, CacheException, KeyNotFoundException{
+                                          Integer mapKey,
+                                          String mapObject)
+            throws DBException, CacheException, KeyNotFoundException, ConfigException {
 
         MAP_Coord_FeatureState state = new MAP_Coord_FeatureState();
         state.setMapKey(mapKey);
         state.setMGITypeKey(MGITypeKey);
         String oId = rawAttr.getObjectId();
-        Integer objectKey = accLookup.lookup(oId);
+        Integer objectKey = null;
+        logger.logdDebug("coordCfg.getMapCollectionName: " + coordCfg.getMapCollectionName());
+        if (coordCfg.getMapCollectionName().equals("NCBI Gene Model")) {
+            // sequences is a set of KeyValue objects key=seqKey, value=chromosome
+            logger.logdDebug("using ncbiLookup");
+            HashMap sequences = ncbiLookup.lookup(oId);
+            for (Iterator mapI = sequences.keySet().iterator(); mapI.hasNext();) {
+                Integer seqKey = (Integer) mapI.next();
+                String chromosome = (String) sequences.get(seqKey);
+                logger.logdDebug("seqKey: " +  seqKey + " chromosome: " + chromosome + " mapObject: " + mapObject);
+                if (mapObject.equals(chromosome)) {
+                    objectKey = seqKey;
+                }
+            }
+        }
+        else {
+            logger.logdDebug("using accessionLookup");
+            objectKey = accLookup.lookup(oId);
+        }
+
         if(objectKey == null) {
-            throw new KeyNotFoundException(oId, "AccessionLookup");
+            throw new KeyNotFoundException(oId, "AccessionLookup or NCBI Seq Lookup");
         }
         state.setObjectKey(objectKey);
         state.setStartCoordinate(new Double(rawAttr.getStartCoord()));
